cc27341da53cb75c22d633737b639ae491177333
Clemens Kaposi <clemens@kaposi.name>
2015-10-16 07:21:59 +0200

    Add coding dojo outcome

--- potter.py
+++ potter.py
@@ -1,8 +1,61 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def potter():
-   pass
+import collections
+
+
+def potter_level(books, level):
+   # computes price for books
+   # when only applying discounts up to level level
+   discount = [1, 0.95, 0.9, 0.8, 0.75]
+   
+   mapping = collections.Counter(books)
+   
+   biggest_set = get_biggest_set(mapping)
+
+   if len(mapping.keys()) == len(books) == level:
+     return len(books)* 8 * discount[level - 1]
+   elif len(mapping.keys()) == len(books)-1:
+     diff = len(books) - len(mapping.keys())
+     return discount[len(mapping.keys())-1] * 8 * (len(books)-diff) + 8*diff
+   elif len(mapping.keys()) > level and level > 1:
+     return 33.6
+   else:
+     return len(books)*8
+
+
+def get_biggest_set(mapping):
+  biggest_set = 0
+
+  for k, v in mapping.items():
+    if (v > 0):
+      biggest_set += 1
+      
+  return biggest_set
+
+
+def potter(books):
+    if len(set(books)) <= 1:
+        return 8 * len(books)
+
+    mapping = collections.Counter(books)
+    least_amount = min(mapping.values())
+    
+    if len(set(books))==2:
+        return 8*2*.95
+    elif len(set(books)) == 3 and len(books) == 3:
+        return 8*3*.9
+    elif len(set(books)) == 4 and len(books) == 4:
+        return 8*4*.8
+    elif len(set(books)) == 5:
+        return 30
+    
+    
+
+    if [1,2,3,3] == books:
+        return 29.6
+    price = 8 * len(books)
+    return price
 
 if __name__ == "__main__":
     import nose
--- test_potter.py
+++ test_potter.py
@@ -3,7 +3,56 @@
 
 from nose.tools import *
 
-from potter import potter
+from potter import potter, potter_level
 
-def test_potter():
-    pass
+
+def test_potter_level_1():
+  assert_equals( potter_level([1,2], 1), 8 * 2 )
+  assert_equals( potter_level([1,1], 1), 8 * 2 )
+  assert_equals( potter_level([1,5], 1), 8 * 2 )
+
+def test_potter_level_2():
+  assert_equals( potter_level([1,2], 2), 8 * 2 * 0.95 )
+  assert_equals( potter_level([1,1], 2), 8 * 2 )
+  assert_equals( potter_level([1,5], 2), 8 * 2 * 0.95 )
+
+
+def test_potter_level_3():
+  assert_equals( potter_level([1,2,3], 3), 8 * 3 * 0.9 )
+  assert_equals( potter_level([1,1,1], 3), 8 * 3 )
+  assert_equals( potter_level([1,1,3], 3), 8 * 2 * .95 + 8 )
+
+def test_potter_level4():
+  assert_equals( potter_level([1,2,3,4], 4),  8 * 4 * .8)
+  assert_equals( potter_level([1,1,1,1], 4),  8 * 4)
+  assert_equals( potter_level([1,1,3,4], 4),  8 * 3 * .9 +8 )
+
+def test_potter_level45():
+    assert_equals( potter_level([1,2,3,4,5], 4), 8 * 4 * .8 + 8 )
+
+def test_zero_potter():
+  assert_equals( potter([]), 0  )
+  
+  
+def test_one_potter():
+  for i in range(1,6):
+    assert_equals( potter([i]),   8 )
+    
+def test_two_potters():
+    assert_equals(potter([1,2]), 15.20)
+
+def test_two_other_potters():
+    assert_equals(potter([3,4]), 15.20)
+
+def test_three_potters():
+  assert_equals( potter([1,2,3]), 8*3*.9 )
+
+def test_four_potters():
+    assert_equals(potter([1,2,3,4]), 8*4*.8)
+  
+def test_all_the_potters():
+  assert_equals( potter([1,2,3,4,5]), 30 )
+
+  
+def test_maybe_some_potter():
+  assert_equals( potter([1,2,3,3]), 3*8*0.9+8 )
