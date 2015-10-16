20216f37462f6d24a7d28e0c0a70d4b34179c549
Clemens Kaposi <clemens@kaposi.name>
2015-10-16 07:21:04 +0200

    Add coding dojo base files

---
+++ potter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def potter():
+   pass
+
+if __name__ == "__main__":
+    import nose
+    nose.main()
---
+++ test_potter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from nose.tools import *
+
+from potter import potter
+
+def test_potter():
+    pass
